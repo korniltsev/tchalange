f1cefcd7058a6d3873b5756ee582a77d4e7e1cdc
Anatoly Korniltsev <korniltsev.anatoly@gmail.com>
2015-08-12 19:21:11 +0300

    miniplayer init

---
+++ core/src/main/java/ru/korniltsev/telegram/core/audio/MiniPlayerView.java
@@ -0,0 +1,19 @@
+package ru.korniltsev.telegram.core.audio;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class MiniPlayerView extends LinearLayout {
+
+    public MiniPlayerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+    }
+}
